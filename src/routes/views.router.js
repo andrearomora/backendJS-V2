a40bdcc6c7260c1abcbeed5486ce063acfd35bb0
andrearomora <andreaesromora@gmail.com>
2023-11-23 00:06:32 -0500

    cart view

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -12,38 +12,11 @@ router.get('/', async (req, res) => {
 router.get('/login', async (req, res) => {
     res.render('login', {})
 })
+router.get('/cart', async (req, res) => {
+    res.render('cart', {})
+})
 
-router.get('/shop', async (req, res) => {
-    
-    // const limit = parseInt(req.query?.limit || 10) 
-    // const page = parseInt(req.query?.page || 1) 
-    // const sort = parseInt(req.query?.sort)
-    // const queryParams = req.query?.query || ''
-    // const query = {}
-
-    // if (queryParams){
-    //     const field = queryParams.split(',')[0]
-    //     const value = queryParams.split(',')[1]
-
-    //     if(!isNaN(parseInt(value))) value = parseInt(value)
-
-    //     query[field] = value
-
-    // }
-    // //const cartID = await cartModel.findOne({})
-
-    // const result = await getProducts.paginate(query, {
-    //     page,
-    //     limit,
-    //     sort,
-    //     lean: true
-    // })
-    
-    // result.prevLink =  result.hasPrevPage ? `/products/?page=${result.prevPage}&limit=${limit}` : ''
-    // result.nextLink =  result.hasNextPage ? `/products/?page=${result.nextPage}&limit=${limit}` : ''
-    // result.user =  req.session.user
-    // result.admin = false
-    // if(req.session.user.rol == "admin") result.admin = true
+router.get('/shop', passportJWT(), async (req, res) => {
     const products = await productService.getProducts()
     res.render('shop', {products})
     console.log(JSON.stringify(products))
