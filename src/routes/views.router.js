6523f7078d0e938ce9b17558168657874dc67a76
andrearomora <andreaesromora@gmail.com>
2023-11-23 23:11:31 -0500

    Product detail page

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -10,17 +10,51 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/login', async (req, res) => {
+    if (req.session?.user) {
+        res.redirect('/')
+    }
     res.render('login', {})
 })
 router.get('/cart', async (req, res) => {
     res.render('cart', {})
 })
 
-router.get('/shop', passportJWT(), async (req, res) => {
-    const products = await productService.getProducts()
-    res.render('shop', {products})
-    console.log(JSON.stringify(products))
+router.get('/shop', async (req, res) => {
+    const sort = parseInt(req.query?.sort)
+    let result = {}
 
+    if (req.session?.user) {
+        result = {
+            user: req.session.user,
+            sort,
+            products: await productService.getProducts()
+        }
+    } else {
+        result = {
+            sort,
+            products: await productService.getProducts()
+        }
+    }
+    res.render('shop', {result})
+})
+
+router.get('/product/:pid', async (req, res) => {
+
+    const id = req.params?.pid
+    const prod = await productService.getProductById(id)
+    
+    const result = {
+        thumbnail: prod.thumbnail,
+        stock: prod.stock,
+        title: prod.title,
+        description: prod.description,
+        price: prod.price
+    }
+
+    result.cantEdit = false
+
+    console.log(result)
+    res.render('productDetail', {result})
 })
 
 export default router
